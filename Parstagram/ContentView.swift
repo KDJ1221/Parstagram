1b8ef6d6eedf579ea69835b60997209e72415acf
Kimmiko James <kimmikojames@gmail.com>
2019-11-11 21:50:24 -0800

    Initial Commit

---
+++ Parstagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Parstagram
+//
+//  Created by Miko James on 11/11/19.
+//  Copyright © 2019 Miko James. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
