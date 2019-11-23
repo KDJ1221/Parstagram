8e6763f7d4011ec345d5c049ead6e4bc482e0964
Kimmiko James <kimmikojames@gmail.com>
2019-11-22 23:40:53 -0800

    Adding comment features

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -11,9 +11,7 @@ import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Parse.initialize(
@@ -22,7 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://salty-reef-11362.herokuapp.com/parse"
             })
         )
-        
         return true
     }
 
